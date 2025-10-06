14aba979696c677c9a6b43cfecfba44054f48964
sebkk <sgrabczak01@gmail.com>
2025-10-06 17:37:32 +0200

    feat: Enhance middleware for dynamic routing and update styles
    - Implemented custom middleware in Next.js to handle dynamic redirects for product categories and product details based on the request pathname.
    - Updated styles in ContentHTML and ProductMainSection components to improve typography and hover effects, ensuring better visual consistency.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,48 @@
+import { NextRequest, NextResponse } from 'next/server';
+
 import createMiddleware from 'next-intl/middleware';
 
+import { DEFAULT_LOCALE, PATHNAMES } from './constants/locales';
+import { NAVIGATION_ROUTE } from './constants/navigation';
 import { routing } from './i18n/routing';
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing);
+
+export default function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  let newPathname = pathname;
+
+  if (pathname.startsWith('/en')) {
+    newPathname = pathname.replace('/en', '');
+  }
+
+  if (newPathname.startsWith('/product-category')) {
+    const pathSplitted = newPathname
+      .replace('/product-category', '')
+      .split('/');
+
+    const pathWithoutPrefix = pathSplitted[pathSplitted.length - 1];
+
+    const path = PATHNAMES[NAVIGATION_ROUTE.PRODUCTS_LISTING_CATEGORY][
+      DEFAULT_LOCALE
+    ].replace('[categoryName]', pathWithoutPrefix);
+
+    return NextResponse.redirect(new URL(path, request.url));
+  }
+
+  if (newPathname.startsWith('/product')) {
+    const pathWithoutPrefix = newPathname.replace('/product', '');
+
+    const path = PATHNAMES[NAVIGATION_ROUTE.PRODUCT_DETAILS][
+      DEFAULT_LOCALE
+    ].replace('[productSlug]', pathWithoutPrefix);
+
+    return NextResponse.redirect(new URL(path, request.url));
+  }
+
+  return intlMiddleware(request);
+}
 
 export const config = {
   matcher: [
